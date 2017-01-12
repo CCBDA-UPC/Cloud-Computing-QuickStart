8138e06bd25526b6ed0ae0fbfa56e2553ecca047
Jordi Torres <torres@ac.upc.edu>
2017-01-12 18:02:30 +0100

    Create GuessNumber.py

---
+++ GuessNumber.py
@@ -0,0 +1,14 @@
+import random
+randomNumber = random.randrange(0,20)
+print("Random number between 0 and 20 has been generated")
+guessed = False
+while guessed==False:
+    userNumber = int(input("Introduce your guess number: "))
+    if userNumber==randomNumber:
+        guessed = True
+        print("Excellent, Well done!")
+    elif userNumber>randomNumber:
+        print("Try one more time, a bit lower")
+    elif userNumber < randomNumber:
+        print("Try one more time, a bit higher")
+print("Yes, you can!")
