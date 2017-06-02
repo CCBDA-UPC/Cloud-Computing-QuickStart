0f7deb9d739457478e70fa97b7c453998f24764f
Angel Toribio <angeltoribio-UPC-BCN@users.noreply.github.com>
2017-06-02 17:33:12 +0200

    Update GuessNumber.py

--- GuessNumber.py
+++ GuessNumber.py
@@ -3,7 +3,7 @@ randomNumber = random.randrange(0,20)
 print("Random number between 0 and 20 has been generated")
 guessed = False
 while guessed==False:
-    userNumber = int(input("Introduce your guess number: "))
+    userNumber = int(input("Take a guess: "))
     if userNumber==randomNumber:
         guessed = True
         print("Excellent, Well done!")
